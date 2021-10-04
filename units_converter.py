9c50221ed397305d7878d5a699564732155807b3
immbiochem <pfft@mail.ru>
2021-10-04 18:33:17 +0500

    Add file units_converter.py

---
+++ units_converter.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+def cell_count_converter(cell_count):
+    print("Your cell count in 1 ml:", cell_count*10000)
+while True:
+    command = input("Enter your command (cell_count or exit):")
+    if command == "exit":
+        print("That's all")
+        break
+    elif command == "cell_count":
+        cell_in_chamber = int(input("Enter the number of cells in 25 large squares of Goryaev's chamber:"))
+        cell_count_converter(cell_in_chamber)
+        print("Next step")
+        continue
+    else:
+        print("This command is incorrect, please, enter correct command")
+        continue       
+
+
+# In[ ]:
+
+
+
+
